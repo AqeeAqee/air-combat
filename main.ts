91cf014c3b4c9b8fd032af818138da69a38ebbc9
AqeeAqee <aqee@msn.com>
2023-09-08 23:56:36 +0800

    feature list

--- main.ts
+++ main.ts
@@ -1,59 +1,61 @@
 game.stats = true
 const hardcore: boolean = false //game.ask("Hardcore mode?");
 
-//test
-// summary.destroiedEnemy(Enemies.redPlane.create({ v: 10, pos: 10, direction: Direction.DOWN }), null)
-// summary.destroiedEnemy(Enemies.redPlane.create({ v: 10, pos: 10, direction: Direction.DOWN }), null)
-// summary.show()
-
 // light.setBrightness(7);
 // light.setLength(5);
 scene.setBackgroundColor(9);
 
 StoryBook.play();
 
-//////////////////////////////////
-/** Change Log, Aqee Li
-2023-8
-add lightning for bomb
-spawn plane on carrier
-spawn helicopter on battle ship
-scored by the player hit more, intead of last shot
-fix btn events
-unified enemy scores
-custom Image Dialog, with scolling
-add level summary, for both players, scoll with arrow keys
-add island for ground-enemies, in randomLevels
-fix max 4 bomb to 3
-2022-1
-fix copilot z-index over pilot when following
-2022-12
-adds/changes:
-always 2 plyers on sim or RPi
-add title scene (handdrawing)
-add progress bar, diff colors for each kind of enemies
-change 2nd player plane to green
-co-pilot mode(removed player count choose), effect whenever 2nd player without input for 5s, press any btn exit.
-co-pilot will never die
-add player's spawn/ level begin/level complete behaviors with animations
-add new weapon, yellow icon, bullets concentrated
-add 5th weapon level, extra bullets shooting backward
-anymore powerup at 5th weapon level, will kill all-in-screen, like a bomb.
-2s ghost time after spawn
-add hitting sound, even didn't destroyed enemy
-change powerups droping from screen top
-pump away from enemy instead of random direction
-pump away occured only be hit but didn't dead
-commented hardcore mode
+/** 
+features:
+ 
+ * Players:
+     * always 2 plyers (orange and green) on sim or RPi
+     * player's spawn/level complete animations
+     * 2s ghost time after spawn
+     * co-pilot mode 
+        * whenever 2nd player absence for 5s, press any key to exit absence.
+        * co-pilot will never die
+        * removed player count choosing
+        * P1 drop P2 bomb if P1's used out
+     * pump away from enemy instead of random direction
+     * pump away occured only be hit but didn't dead    
 
-experience features:
-random generated level.
+ * Weapon:
+     * add new weapon, yellow P icon, bullets concentrated
+     * add 5th weapon level, extra bullets shooting backward
+     * got powerup at 5th weapon level, drop an extra bomb immdiately.
+     * powerups icons dropping changed to from screen top
+
+ * Bomb:
+     * drop bomb with lightning effect
+
+ * Enemy:
+     * add airplane carrier
+     * spawn plane on carrier
+     * spawn helicopter on battle ship
+     * unified enemy scores
+     * hitting sound, even enemy hasn't destroyed
+
+ * Levels:
+     * add endless random generated levels after 7 designed levels.
+
+ * UI:
+     * add title scene (handdrawing)
+     * add progress bar, diff colors for each kind of enemies
+     * add level summary, custom Image Dialog
+        * for both players, scoll with arrow keys
+        * scored by the player hit more, intead of who shot last bullet
+     * commented hardcore mode choosing
 
 fixed:
-bigplane missing when moving left/right: w/h should swap after transposed image
-perf tune, avoid set fire effect of enemy repeatly
+ * bigplane missing when moving left/right: w/h should swap after transposed image
+ * perf tune, avoid set fire effect of enemy repeatly
+ * fix max 4 bomb to 3
 
 TODO:
-load only one level a time, for save memory of device
-tune FPS.
+ * load only one level a time, for save memory of device
+ * tune FPS.
+
  */
